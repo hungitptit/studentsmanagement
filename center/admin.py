8731db67f21392e364d6875549ffaaa57c64c1d8
hungitptit <61471882+hungitptit@users.noreply.github.com>
2022-01-24 09:41:49 +0700

    update

--- center/admin.py
+++ center/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(UserProfile)
-
+admin.site.register(Class)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Test)
+admin.site.register(Result)
 # Register your models here.
